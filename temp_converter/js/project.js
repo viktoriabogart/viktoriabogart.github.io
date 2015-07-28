207acbfc37daea2a8b85a976743269b97716c96c
viktoriabogart <viktoriaabogart@Macintosh.fios-router.home>
2015-07-28 13:35:52 -0400

    temp-converter

---
+++ temp_converter/js/project.js
@@ -0,0 +1,48 @@
+$(function() {
+
+	function convertC(){
+
+	var f = $('.userTempF').val();
+
+ 	f = parseInt(f);
+
+ 	var c = (f-32) * 5/9; 
+
+ 	var result = Math.round(c*10)/10;
+
+ 	$('.cresult').html(result + "\xB0 C");
+
+
+ }
+
+ function convertF(){
+
+ 	var c = $('.userTempC').val();
+
+ 	c = parseInt(c);
+
+ 	var f = (c*9)/5 + 32;
+ 	
+ 	var result = Math.round(f*10)/10;
+
+ 	$(".fresult").html(result + "\xB0 F");
+
+ }
+
+ 	
+
+
+$('.converter').click(convertC);
+
+$('.converter2').click(convertF);
+
+
+
+
+ 
+
+
+
+});
+
+
